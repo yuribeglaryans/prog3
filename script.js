7229161910108360ad8d2c059186ad14b978ab5c
yuribeglaryans <yuri.beglaryan.s@tumo.org>
2023-02-11 18:02:46 +0400

    inheritance

--- script.js
+++ script.js
@@ -1,2 +1,14 @@
-console.log("yay")
-alert("hi")
+var obj = {
+    "first_name": "Karo",
+    "last_name": "Hovsepyan",
+    "age": 13,
+    "tumo_student": false,
+
+    sayHello() {
+        console.log(this.last_name);
+    }
+}
+
+obj.sayHello();
+//  console.log(obj.first_name);
+//  obj.sayHello();
